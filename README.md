69fbee9e878c1c610c1b44653d7260e055213c38
warmek <w4rmek@proton.me>
2026-04-05 15:47:23 +0000

    Populate database

---
+++ README.md
@@ -0,0 +1,8 @@
+# BGD
+
+## [Datasource](https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+
+Yellow and green taxi trip records include fields capturing pickup and drop-off dates/times, pickup and drop-off locations, trip distances, itemized fares, rate types, payment types, and driver-reported passenger counts. The data used in the attached datasets were collected and provided to the NYC Taxi and Limousine Commission (TLC) by technology providers authorized under the Taxicab & Livery Passenger Enhancement Programs (TPEP/LPEP). The trip data was not created by the TLC, and TLC makes no representations as to the accuracy of these data.
+
+### [Data dictionary](https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_hvfhs.pdf)
+
---
+++ scripts/load_database.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv, dotenv_values
+import psycopg2 as pc
+from sqlalchemy import create_engine
+from pathlib import Path
+
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT", 5432)
+database = "bgd"
+
+# print(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+#
+# engine = create_engine(
+#         f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+#     )
+
+
+for year in os.listdir("data/"):
+    Path(f"buff/csv/").mkdir(parents=True, exist_ok=True)
+    for month_parquet in os.listdir(f"data/{year}/"):
+        df = pd.read_parquet(f"data/{year}/{month_parquet}", engine="pyarrow")
+        print(f'Save {month_parquet} to csv')
+        df.to_csv(f"buff/csv/{month_parquet}.csv")
+
+# with pc.connect(dbname=database, user=user, password=password, host=host, port=port) as conn:
+#     with conn.cursor() as cursor:
+#
+#         for year in os.listdir("data/"):
+#             for month_parquet in os.listdir(f"data/{year}/"):
+#                 df = pd.read_parquet(f"data/{year}/{month_parquet}", engine="pyarrow")
+#                 size = df.size
+#                 n=100_000
+#                 list_df = [df[i:i+n] for i in range(0,len(df),n)]
+#
+#                 for chunk_df in list_df:
+#                     chunk_df.to_sql(name='trips', con=engine, if_exists='append', method='multi')
+#                     print(f"Inserted {n}/{size} - {(n/size)*100}")
+#
+#                     conn.commit()
+#
+#         conn.close()
