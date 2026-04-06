89ca867d04274a02dcbf93d8e7c14536ebdb8c47
warmek <w4rmek@proton.me>
2026-04-06 16:08:55 +0000

    Clean data

---
+++ scripts/load_database_clean.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+from pathlib import Path
+
+for year in os.listdir("data/"):
+    Path(f"buff/cleaned_csv/").mkdir(parents=True, exist_ok=True)
+    for month_parquet in os.listdir(f"data/{year}/"):
+        df = pd.read_parquet(f"data/{year}/{month_parquet}", engine="pyarrow")
+        mappings = {
+            'Y': True,
+            'N': False
+        }
+        df['shared_request_flag'] = df['shared_request_flag'].map(lambda s: mappings[s])
+        df['shared_match_flag'] = df['shared_match_flag'].map(lambda s: mappings[s])
+        df['access_a_ride_flag'] = df['access_a_ride_flag'].map(lambda s: mappings[s])
+        df['wav_request_flag'] = df['wav_request_flag'].map(lambda s: mappings[s])
+        df['wav_match_flag'] = df['wav_match_flag'].map(lambda s: mappings[s])
+        print(df.head())
+        print(f'Save {month_parquet} to csv')
+        df.to_csv(f"buff/cleaned_csv/{month_parquet}_cleared.csv")
+
